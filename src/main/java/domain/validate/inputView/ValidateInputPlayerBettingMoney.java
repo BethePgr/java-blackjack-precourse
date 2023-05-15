092e3a317c89f7ed749bc25607cf6876b1325ed9
SEC <c3392302@naver.com>
2023-05-15 14:14:00 +0900

    feat: 플레이어 당 배팅 금액 입력받는 기능 유효성 검사

---
+++ src/main/java/domain/validate/inputView/ValidateInputPlayerBettingMoney.java
@@ -0,0 +1,31 @@
+package domain.validate.inputView;
+
+public class ValidateInputPlayerBettingMoney {
+
+    public static int validateBettingMoney(String input){
+        int money = onlyNumber(input);
+        MoneyIsMoreThan1000(money);
+        MoneyCanDivideBy1000(money);
+        return money;
+    }
+
+    private static int onlyNumber(String input){
+        String reg = "^[0-9]+$";
+        if(input.matches(reg)){
+            return Integer.parseInt(input);
+        }
+        throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+    }
+
+    private static void MoneyIsMoreThan1000(int money){
+        if(money < 1000){
+            throw new IllegalArgumentException("[ERROR] 배팅 금액은 천원 이상이여야 합니다.");
+        }
+    }
+
+    private static void MoneyCanDivideBy1000(int money){
+        if(money % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 배팅 금액은 천원 단위로 나누어 떨어져야 합니다.");
+        }
+    }
+}
