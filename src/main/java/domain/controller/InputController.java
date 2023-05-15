2721483fa2d12d0ec95332480708f9726ce6aa85
SEC <c3392302@naver.com>
2023-05-15 14:03:25 +0900

    fix: InputController의 inputPlayerNames가 반환하는 객체를 String에서 쉼표로 구분된 List<String>으로 변경

--- src/main/java/domain/controller/InputController.java
+++ src/main/java/domain/controller/InputController.java
@@ -2,14 +2,17 @@ package domain.controller;
 
 import domain.validate.inputView.ValidateInputPlayersName;
 import domain.view.InputView;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputController {
 
-    public static String inputPlayerNames(){
+    public static List<String> inputPlayerNames(){
         try {
             String input = InputView.inputPlayersName();
             ValidateInputPlayersName.validatePlayersName(input);
-            return input;
+            return Arrays.stream(input.split(",")).collect(Collectors.toList());
         }catch (IllegalArgumentException e){
             System.out.println(e.getMessage());
             return inputPlayerNames();
