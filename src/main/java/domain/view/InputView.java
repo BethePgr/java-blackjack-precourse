317f27eb54938b89524461adabc485baaa439985
SEC <c3392302@naver.com>
2023-05-15 14:13:50 +0900

    feat: 플레이어 당 배팅 금액 입력받는 기능 구현

--- src/main/java/domain/view/InputView.java
+++ src/main/java/domain/view/InputView.java
@@ -10,4 +10,9 @@ public class InputView {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
         return scanner.next();
     }
+
+    public static String inputPlayerBettingMoney(String name){
+        System.out.println(name + "의 배팅 금액은?");
+        return scanner.next();
+    }
 }
