34b416069c59484788379f95906118e529720f3a
SEC <c3392302@naver.com>
2023-05-16 13:10:32 +0900

    feat: 게임 계속할건지 묻는 입력 받는 기능 유효성 검사

---
+++ src/main/java/domain/validate/inputView/ValidateInputPlayerContinueGame.java
@@ -0,0 +1,11 @@
+package domain.validate.inputView;
+
+public class ValidateInputPlayerContinueGame {
+
+    public static void validateContinue(String input){
+        if(!input.equals("y") && !input.equals("n")){
+            throw new IllegalArgumentException("[ERROR] y나 n만 입력해주세요.");
+        }
+    }
+
+}
