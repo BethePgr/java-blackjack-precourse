a9d211a12bfe4f9a8932d755595eade0340d5053
SEC <c3392302@naver.com>
2023-05-16 13:05:57 +0900

    feat: 점수 구하는 기능 구현

--- src/main/java/domain/model/user/Dealer.java
+++ src/main/java/domain/model/user/Dealer.java
@@ -2,23 +2,50 @@ package domain.model.user;
 
 import domain.model.card.Card;
 
+import domain.model.card.Symbol;
+import domain.model.card.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 게임 딜러를 의미하는 객체
  */
 public class Dealer {
-    private final List<Card> cards = new ArrayList<>();
 
-    public Dealer() {}
+    private final List<Card> cards = new ArrayList<>();
+    private int score = 0;
+    private boolean haveAceCard;
+
+    public Dealer() {
+    }
 
     public void addCard(Card card) {
         cards.add(card);
+        addScore(card);
+        haveAceCard = aceCard();
     }
 
     // TODO 추가 기능 구현
-    public List<Card> getCards(){
+    public List<Card> getCards() {
         return cards;
+    }
+
+    private void addScore(Card card){
+        score+= card.getSymbol().getScore();
+    }
+
+    private boolean aceCard(){
+        return cards.stream().anyMatch(card -> card.getSymbol() == Symbol.ACE);
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public boolean isHaveAceCard(){
+        return haveAceCard;
     }
 }
--- src/main/java/domain/model/user/Player.java
+++ src/main/java/domain/model/user/Player.java
@@ -2,7 +2,9 @@ package domain.model.user;
 
 import domain.model.card.Card;
 
+import domain.model.card.Symbol;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -12,6 +14,8 @@ public class Player {
     private final String name;
     private final double bettingMoney;
     private final List<Card> cards = new ArrayList<>();
+    private int score = 0;
+    private boolean haveAceCard;
 
     public Player(String name, double bettingMoney) {
         this.name = name;
@@ -20,6 +24,8 @@ public class Player {
 
     public void addCard(Card card) {
         cards.add(card);
+        addScore(card);
+        haveAceCard = aceCard();
     }
 
     // TODO 추가 기능 구현
@@ -29,5 +35,21 @@ public class Player {
 
     public String getName(){
         return name;
+    }
+
+    private void addScore(Card card){
+        score+= card.getSymbol().getScore();
+    }
+
+    private boolean aceCard(){
+        return cards.stream().anyMatch(card -> card.getSymbol() == Symbol.ACE);
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public boolean isHaveAceCard(){
+        return haveAceCard;
     }
 }
