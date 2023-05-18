9e6de28d2b7895707c61cf999f409a47afb1e8bd
jongwonpc <c3392302@naver.com>
2023-05-18 20:29:45 +0900

    refactor: 컨트롤러 리팩토링

--- src/main/java/domain/controller/BlackJackController.java
+++ src/main/java/domain/controller/BlackJackController.java
@@ -11,35 +11,53 @@ import java.util.List;
 public class BlackJackController {
 
     public static void main(String[] args) {
-        List<String> names = InputController.inputPlayerNames();
-        List<Player> playerLists = createPlayerLists(names);
+
+        List<Player> playerLists = createPlayers();
         BlackJackService blackJackService = new BlackJackService(playerLists);
         OutputView.printAllCardsOfDealerAndPlayers(blackJackService);
         for(Player player : playerLists){
-            String input;
-            do{
-                input = InputController.inputPlayerContinueGame(player.getName());
-                blackJackService.addOneMoreCardToPlayer(player,input);
-                if(blackJackService.isPlayerScoreOver21(player)){
-                    OutputView.showPlayerCards(player);
-                    OutputView.alreadyScoreOver21(player);
-                    break;
-                }
-                if(input.equals("y")) {
-                    OutputView.showPlayerCards(player);
-                }
-            }while(input.equals("y"));
+            continuePlayerAddCard(blackJackService,player);
         }
-        if(blackJackService.isDealerScoreUnder16()) {
-            blackJackService.addOneMoreCardToDealer();
-            OutputView.dealerAddOneMoreCard();
-        }
+        printIfDealerScoreUnder16(blackJackService);
+        printResultOfGame(blackJackService);
+    }
+
+    private static void continuePlayerAddCard(BlackJackService blackJackService,Player player){
+        String input;
+        do{
+            input = InputController.inputPlayerContinueGame(player.getName());
+            blackJackService.addOneMoreCardToPlayer(player,input);
+            if(blackJackService.isPlayerScoreOver21(player)){
+                OutputView.showPlayerCards(player);
+                OutputView.alreadyScoreOver21(player);
+                break;
+            }
+            if(input.equals("y")) {
+                OutputView.showPlayerCards(player);
+            }
+        }while(input.equals("y"));
+    }
+
+    private static void printResultOfGame(BlackJackService blackJackService){
         OutputView.printAllCardsAndScoreOfDealerAndPlayers(blackJackService);
         blackJackService.makeResult();
         OutputView.showAllResult(blackJackService);
     }
 
-    private static List<Player> createPlayerLists(List<String> names) {
+    private static void printIfDealerScoreUnder16(BlackJackService blackJackService){
+        if(blackJackService.isDealerScoreUnder16()) {
+            blackJackService.addOneMoreCardToDealer();
+            OutputView.dealerAddOneMoreCard();
+        }
+    }
+
+    private static List<Player> createPlayers(){
+        List<String> names = InputController.inputPlayerNames();
+        return createStringToPlayerLists(names);
+    }
+
+
+    private static List<Player> createStringToPlayerLists(List<String> names) {
         List<Player> playerList = new ArrayList<>();
         for(String name : names){
             int money = InputController.inputPlayerBettingMoney(name);
